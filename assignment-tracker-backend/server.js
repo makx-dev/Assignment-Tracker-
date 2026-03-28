bfb283dfc024dc1c69acbd3cbef8b409a512ba7f
MAKX Dev <manthankhotele7@gmail.com>
2026-03-28 22:05:43 +0530

    Fix backend MongoDB connection options and startup entrypoint

--- assignment-tracker-backend/server.js
+++ assignment-tracker-backend/server.js
@@ -15,12 +15,9 @@ app.use(express.json());
 
 const mongoUri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/assignment-tracker';
 
-mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(mongoUri)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 app.get('/', (req, res) => {
     res.send('Assignment Tracker Backend is running.');
