dbe12b1677d7ad53a5cf957ed91c9bff10a8e9d7
MAKX Dev <manthankhotele7@gmail.com>
2026-03-31 14:15:23 +0530

    Connect frontend to Render backend

--- assignment-tracker-backend/src/api.js
+++ assignment-tracker-backend/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://assignment-tracker-kwfv.onrender.com/';
+const BASE_URL = 'https://assignment-tracker-kwfv.onrender.com/api';
 
 // Students
 export const getAllStudents = async () => {
